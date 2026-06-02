4a02d6e371fd13a9d4df4246f98fdb2d04c255e7
danielfaloshey <dmfaloshey@student.fullsail.edu>
2026-06-02 13:28:33 -0400

    UI for inventory and settings fragments

--- app/src/main/java/com/faloshey/chorechampion/fragments/parent/ParentInventoryFragment.java
+++ app/src/main/java/com/faloshey/chorechampion/fragments/parent/ParentInventoryFragment.java
@@ -2,5 +2,9 @@ package com.faloshey.chorechampion.fragments.parent;
 
 import androidx.fragment.app.Fragment;
 
+import com.faloshey.chorechampion.R;
+
 public class ParentInventoryFragment extends Fragment {
+
+    public ParentInventoryFragment() { super(R.layout.fragment_inventory); }
 }
--- app/src/main/java/com/faloshey/chorechampion/fragments/parent/ParentSettingsFragment.java
+++ app/src/main/java/com/faloshey/chorechampion/fragments/parent/ParentSettingsFragment.java
@@ -2,5 +2,9 @@ package com.faloshey.chorechampion.fragments.parent;
 
 import androidx.fragment.app.Fragment;
 
+import com.faloshey.chorechampion.R;
+
 public class ParentSettingsFragment extends Fragment {
+
+    public ParentSettingsFragment() { super(R.layout.fragment_settings); }
 }
