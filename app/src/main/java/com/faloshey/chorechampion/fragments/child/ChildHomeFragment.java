5df48231ec1d73f4ffe7fa745d0778f41d184fcc
danielfaloshey <dmfaloshey@student.fullsail.edu>
2026-06-04 13:24:02 -0400

    UI for child home and quests fragments

--- app/src/main/java/com/faloshey/chorechampion/fragments/child/ChildHomeFragment.java
+++ app/src/main/java/com/faloshey/chorechampion/fragments/child/ChildHomeFragment.java
@@ -2,5 +2,11 @@ package com.faloshey.chorechampion.fragments.child;
 
 import androidx.fragment.app.Fragment;
 
+import com.faloshey.chorechampion.R;
+
 public class ChildHomeFragment extends Fragment {
+
+    public ChildHomeFragment() { super(R.layout.fragment_childhome); }
+
+
 }
--- app/src/main/java/com/faloshey/chorechampion/fragments/child/ChildQuestsFragment.java
+++ app/src/main/java/com/faloshey/chorechampion/fragments/child/ChildQuestsFragment.java
@@ -2,5 +2,9 @@ package com.faloshey.chorechampion.fragments.child;
 
 import androidx.fragment.app.Fragment;
 
+import com.faloshey.chorechampion.R;
+
 public class ChildQuestsFragment extends Fragment {
+
+    public ChildQuestsFragment() { super(R.layout.fragment_childquests); }
 }
